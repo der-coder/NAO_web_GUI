5719639fd0b82dc7204014265fbf106a0be35a54
Isaac Ayala <isaac.ayalaiii@gmail.com>
2016-03-28 15:25:01 -0600

    Moved static files to static folder

--- app.py
+++ app.py
@@ -1,36 +1,30 @@
 #coding: utf-8
 from bottle import route, error, post, get, run, static_file, abort, redirect, response, request, template
 
+# Static Routes
+@get('/<filename:re:.*\.js>')
+def javascripts(filename):
+    return static_file(filename, root='static/js')
+
+@get('/<filename:re:.*\.css>')
+def stylesheets(filename):
+    return static_file(filename, root='static/css')
+
+@get('/<filename:re:.*\.(jpg|png|gif|ico)>')
+def images(filename):
+    return static_file(filename, root='static/img')
+
+@get('/<filename:re:.*\.(eot|ttf|woff|svg)>')
+def fonts(filename):
+    return static_file(filename, root='static/fonts')
+
+
 @route('/')
 @route('/index.html')
 def index():
 #    return '<a href="/hello">Go to Hello World page</a>'
      return static_file('html/index.html',root='.')
 
-@route('/hello')
-def hello():
-    return '<h1>HELLO WOLRD</h1>'
-
-@route('/hello/:name')
-def hello_name(name):
-    page = request.GET.get('page', '1')
-    return '<h1>HELLO %s <br/>(%s)</h1>' % (name, page)
-
-@route('/static/:filename')
-def serve_static(filename):
-    return static_file(filename, root='/home/arthur/workspace/my_python_codes/src/')
-
-@route('/raise_error')
-def raise_error():
-    abort(404, "error...")
-
-@route('/redirect')
-def redirect_to_hello():
-    redirect('/hello')
-
-@route('/ajax')
-def ajax_response():
-    return {'dictionary': 'you will see ajax response right? Content-Type will be "application/json"'}
 
 @error(404)
 def error404(error):
