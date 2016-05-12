ef86c73ec5b04ad1ca1179d1ab11dadeab589502
Isaac Ayala <isaac.ayalaiii@gmail.com>
2016-05-12 17:28:40 -0500

    Added movement icons x2

--- html/commandMovement.php
+++ html/commandMovement.php
@@ -6,8 +6,8 @@
 <div class="leftside">
         <div class="homebox">
         <ul id="icons">
-<li><a href="?command=SIT_DOWN&params="><img src="html/static/choregraphe-icons/box/movement/sit.png"></a>
-<li><a href="?command=STAND_UP&params="><img src="html/static/choregraphe-icons/box/movement/stand.png"</a>
+<li><a href="?command=SIT_DOWN&params="><img src="static/choregraphe-icons/box/movement/sit.png"></a>
+<li><a href="?command=STAND_UP&params="><img src="static/choregraphe-icons/box/movement/stand.png"></a>
         </ul>
         </div>
 </div>
--- html/control.php
+++ html/control.php
@@ -1,10 +1,6 @@
 <?php include 'connection.php';?>
 <?php include 'header.php';?>
 <body>
-
-<navigation>
-<?php include 'menu.php';?>
-</navigation>
 <?php include 'comandos.php';?>
 <?php include 'footer.php';?>
 
