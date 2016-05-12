d7c0479ba9bd717f257921d1b1493f6ba4b718f8
Isaac Ayala <isaac.ayalaiii@gmail.com>
2016-05-12 17:59:04 -0500

    ITESM color scheme

--- html/header.php
+++ html/header.php
@@ -9,5 +9,7 @@
  <script src="static/js/main.js" type="text/javascript"></script>
  
  <meta charset="UTF-8">
- 
+ <meta name="viewport" content="width=device-width; initial-scale=1.0; maximum-scale=1.0; user-scalable=0;" />
  </head>
+ 
+ 
