b31640110251f6ab59c776adbbbd43e8f5d1daf1
Isaac Ayala <isaac.ayalaiii@gmail.com>
2016-05-12 17:45:22 -0500

    Changed color

--- html/comandos.php
+++ html/comandos.php
@@ -1,4 +1,4 @@
-<ul>
+<ul id="commands">
 <li><a href="index.php">Home</a> </li>
 <li><a id="LED" href="commandLED.php">LED</a>
 <li><a id="Movement" href="commandMovement.php">Movement</a>
--- html/commandVoice.php
+++ html/commandVoice.php
@@ -1,9 +1,5 @@
 <?php include 'header.php';?>
 <body>
-
-<navigation>
-<?php include 'menu.php';?>
-</navigation>
 <?php include 'comandos.php';?>
 <?php include 'footer.php';?>
 
