161ced4af8851289dfa55eb7310bdfd4db4d5ea4
Isaac Ayala <isaac.ayalaiii@gmail.com>
2016-05-12 12:49:41 -0500

    Moved stuff

--- html/index.php
+++ html/index.php
@@ -1,16 +1,14 @@
 <?php include 'connection.php';?>
-
-
 <?php include 'header.php';?>
-<body>
 
 <navigation>
 <?php include 'menu.php';?>
 </navigation>
-<?php include 'comandos.php';?>
+
+<?php include 'home.php';?>
+
 <?php include 'footer.php';?>
 
 </body>
 
 </html>
-
--- html/menu.php
+++ html/menu.php
@@ -1,10 +1,10 @@
 <?php
 echo
  '<ul>
- <li><a href="../index.php">Home</a> </li>
- <li><a href="html/control.php">Control</a> </li>
- <li><a href="html/help.php" target="_blank">Help</a> </li>
- <li class="rightSideLink"><a href="html/about.php">About</a></li>
+ <li><a href="index.php">Home</a> </li>
+ <li><a href="control.php">Control</a> </li>
+ <li><a href="help.php" target="_blank">Help</a> </li>
+ <li class="rightSideLink"><a href="about.php">About</a></li>
  </ul>'
  ;
  
