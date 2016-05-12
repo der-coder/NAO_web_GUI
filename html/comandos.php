8b9efd1314ea14faadf5afd2b210361dec24552f
Isaac Ayala <isaac.ayalaiii@gmail.com>
2016-05-12 12:58:42 -0500

    Minor typo x2

--- html/comandos.php
+++ html/comandos.php
@@ -1,13 +1,11 @@
-<?php echo '
 <ul class="command-classes">
-<li><a href="#" onclick="toggle_visibility('LED'),">LED/a>
-';?>
+<li><a href="#" onclick="toggle_visibility('LED');">LED</a>
 
 <?php include 'commandLED.php' ?>
 
-<?php echo '<li><a href="#" onclick="toggle_visibility('movement');">Movement</a>';?>
+<li><a href="#" onclick="toggle_visibility('movement');">Movement</a>'
 <?php include 'commandVoice.php' ?>
 
-<?php echo '<li><a href="#" onclick="toggle_visibility('voice');">Voice</a>';?>
+<li><a href="#" onclick="toggle_visibility('voice');">Voice</a>'
 <?php include 'commandMovement.php' ?>
-<?php echo '</ul>';?>
+</ul>
