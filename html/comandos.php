a8ba3711c967a4a2554d477c5a97d0a7ecc8abf7
Isaac Ayala <isaac.ayalaiii@gmail.com>
2016-05-12 11:51:18 -0500

    Updated commands in php

--- html/comandos.php
+++ html/comandos.php
@@ -1,34 +1,19 @@
 <?php
 echo 
 '
-<ul>
-<li><a href="?command=CMDGETPROGRAMSTATUS&params="></a>
-<li><a href="?command=CMDMEMORYEVENTADD&params="></a>
-<li><a href="?command=CMDMEMORYEVENTRAISE&params="></a>
-<li><a href="?command=CMDMEMORYEVENTREMOVE&params="></a>
-
+<ul class="commands">
 <li><a href="?command=CMDOPENHAND&params="></a>
-<li><a href="?command=CMDPLAYPROGRAM&params="></a>
+
 <li><a href="?command=CMDSAY&params="></a>
-<li><a href="?command=CMDSETJOINTSTIFFNESS&params="></a>
 <li><a href="?command=CMDSETLIFESTATE&params="></a>
 
 <li><a href="?command=CMDOPENHAND&params="></a>
 <li><a href="?command=CMDPLAYPROGRAM&params="></a>
 <li><a href="?command=CMDSAY&params="></a>
-<li><a href="?command=CMDSETJOINTSTIFFNESS&params="></a>
-<li><a href="?command=CMDSETLIFESTATE&params="></a>
-
-<li><a href="?command=CMDSETNAONAME&params="></a>
-<li><a href="?command=CMDSETPLAYERVOLUME&params="></a>
-<li><a href="?command=CMDSETSPEECHVOICE&params="></a>
-<li><a href="?command=CMDSETSYSTEMVOLUME&params="></a>
 
 <li><a href="?command=CMDSITDOWN&params="></a>
 <li><a href="?command=CMDSTANDUP&params="></a>
 <li><a href="?command=CMDSTOPPROGRAM&params="></a>
-<li><a href="?command=CMDVELOCITYWALK&params="></a>
-
 
 <li><a href="?command=LED_ANGRY&params="></a>
 <li><a href="?command=LED_BLINK&params="></a>
@@ -40,8 +25,34 @@ echo
 <li><a href="?command=LED_HAPPY&params="></a>
 <li><a href="?command=LED_LAUGH&params="></a>
 <li><a href="?command=LED_MISCHIEVIOUS&params="></a>
-<li><a href="?command=LED_SETEYE&params="></a>
 <li><a href="?command=LED_THINKING&params="></a>
 </ul>
 '
 ?>
+
+//<li><a href="?command=CMDGETPROGRAMSTATUS&params="></a>
+//<li><a href="?command=CMDMEMORYEVENTADD&params="></a>
+//<li><a href="?command=CMDMEMORYEVENTRAISE&params="></a>
+//<li><a href="?command=CMDMEMORYEVENTREMOVE&params="></a>
+//<li><a href="?command=CMDPLAYPROGRAM&params="></a>
+//<li><a href="?command=CMDSETJOINTSTIFFNESS&params="></a>
+//<li><a href="?command=CMDSETJOINTSTIFFNESS&params="></a>
+//<li><a href="?command=CMDSETLIFESTATE&params="></a>
+//<li><a href="?command=CMDSETNAONAME&params="></a>
+//<li><a href="?command=CMDSETPLAYERVOLUME&params="></a>
+//<li><a href="?command=CMDSETSPEECHVOICE&params="></a>
+//<li><a href="?command=CMDSETSYSTEMVOLUME&params="></a>
+//<li><a href="?command=CMDVELOCITYWALK&params="></a>
+//<li><a href="?command=LED_SETEYE&params="></a>
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
