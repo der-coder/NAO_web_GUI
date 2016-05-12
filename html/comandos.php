52d9b3ae8033ced29f8cfdf265f6c0ab76956b04
Isaac Ayala <isaac.ayalaiii@gmail.com>
2016-05-12 13:20:22 -0500

    Fixing command display x5

--- html/comandos.php
+++ html/comandos.php
@@ -1,8 +1,10 @@
-<ul class="command-classes">
+<ul>
 <li><a href="#" onclick="toggle_visibility('LED');">LED</a>
 <li><a href="#" onclick="toggle_visibility('movement');">Movement</a>'
 <li><a href="#" onclick="toggle_visibility('voice');">Voice</a>'
 </ul>
+<ul>
 <?php include 'commandLED.php' ?>
 <?php include 'commandVoice.php' ?>
 <?php include 'commandMovement.php' ?>
+</ul>
