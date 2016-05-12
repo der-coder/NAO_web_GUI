4b3573e0d98f53cb568c6a507ecb4eaa4c9c0b63
Isaac Ayala <isaac.ayalaiii@gmail.com>
2016-05-12 13:12:50 -0500

    Fixing command display

--- html/comandos.php
+++ html/comandos.php
@@ -1,11 +1,14 @@
 <ul class="command-classes">
 <li><a href="#" onclick="toggle_visibility('LED');">LED</a>
-
+</ul>
 <?php include 'commandLED.php' ?>
 
+<ul class="command-classes">
 <li><a href="#" onclick="toggle_visibility('movement');">Movement</a>'
 <?php include 'commandVoice.php' ?>
+</ul>
 
+<ul class="command-classes">
 <li><a href="#" onclick="toggle_visibility('voice');">Voice</a>'
 <?php include 'commandMovement.php' ?>
 </ul>
--- html/home.php
+++ html/home.php
@@ -3,7 +3,7 @@
  <h1>Welcome</h1>
  <p title="Introduction">
  This webpage is the frontend of the application. It is meant to be used to be used in conjuction
- with the
+ with the software designed
  </p>
  
  </div>
