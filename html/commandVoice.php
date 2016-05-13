1aedd602b8d1c3e34cac20c745296b32ced072e2
Isaac Ayala <isaac.ayalaiii@gmail.com>
2016-05-12 19:11:13 -0500

    Voice test

--- html/commandVoice.php
+++ html/commandVoice.php
@@ -4,10 +4,10 @@
 <?php include 'banner.php';?>
 <?php include 'comandos.php';?>
 <ul class="voice">
-<li><a href="?command=CMDSAY&params=$parameters"><img src="static/choregraphe-icons/box/interaction/say.png"></a>
+<li><a href="?command=CMDSAY&params="><img src="static/choregraphe-icons/box/interaction/say.png"></a>
 </ul>
   <form action="" method="get">
-    <input name="?command=CMDSAY&params="  value="Hello"/>
+    <input name="?command=CMDSAY&params="  value="?command=CMDSAY&params=Hello"/>
 
     <input type="submit" name="" 
            value=""/>
