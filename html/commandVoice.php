5f187bcbec0db38e3510b6b581ae198b0d12dfae
Isaac Ayala <isaac.ayalaiii@gmail.com>
2016-05-12 20:14:25 -0500

    Changed string

--- html/commandVoice.php
+++ html/commandVoice.php
@@ -4,7 +4,7 @@
 <?php include 'banner.php';?>
 <?php include 'comandos.php';?>
 <ul class="voice">
-<li><a href="?command=SAY&params='Hello Bats'"><img src="static/choregraphe-icons/box/interaction/say.png"></a>
+<li><a href="?command=SAY&params='Good day gentlement, I am NAO. I am being controlled via the Internet.'"><img src="static/choregraphe-icons/box/interaction/say.png"></a>
 </ul>
 <!--<form action="?command=SAY&params=['I believe I can fly']" method="post">
     <input name="parameters" value="['I believe I can fly']"/>
