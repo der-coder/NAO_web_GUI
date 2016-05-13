588c0423b953c50fa75c5bebc68a3e0ddb7bc784
Isaac Ayala <isaac.ayalaiii@gmail.com>
2016-05-12 19:55:27 -0500

    Removed extra icon formatting

--- html/commandMovement.php
+++ html/commandMovement.php
@@ -4,7 +4,7 @@
 <?php include 'banner.php';?>
 <?php include 'comandos.php';?>
 
-        <ul id="icons">
+        <ul>
 <li><a href="?command=SIT_DOWN&params="><img src="static/choregraphe-icons/box/movement/sit.png"></a>
 <li><a href="?command=STAND_UP&params="><img src="static/choregraphe-icons/box/movement/stand.png"></a>
 <li><a id="Seiza" href="?command=SET_LIFE_STATE&params='disabled'"><img src="static/choregraphe-icons/box/wait.png"></a>
