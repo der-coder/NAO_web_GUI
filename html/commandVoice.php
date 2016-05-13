3085fb254a52436511694a3c0fe85289a213412d
Isaac Ayala <isaac.ayalaiii@gmail.com>
2016-05-13 11:45:33 -0500

    Added tongue twister

--- html/commandVoice.php
+++ html/commandVoice.php
@@ -5,7 +5,8 @@
 <?php include 'comandos.php';?>
 <ul class="voice">
 <li><a href="?command=SAY&params='Good day gentlemen, I am NAO. I am being controlled via the Internet.'"><img src="static/choregraphe-icons/box/interaction/say.png"></a>
-<li><a href="?command=LED_DISCO&params=" onclick="myFunction()" id="myCheck" data-role="button" data-icon="alert"><img src="static/choregraphe-icons/box/interaction/say.png"></a>
+
+<li><a onclick="myFunction()" id="myCheck" data-role="button" data-icon="alert"><img src="static/choregraphe-icons/box/interaction/say.png"></a>
 </ul>
 
 <div data-role="fieldcontain">
