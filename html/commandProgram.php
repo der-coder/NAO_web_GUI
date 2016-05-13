20b43462c6670e0b9bbcbc116a58c901a04af452
Isaac Ayala <isaac.ayalaiii@gmail.com>
2016-05-12 19:41:47 -0500

    Removed form

--- html/commandProgram.php
+++ html/commandProgram.php
@@ -8,14 +8,7 @@
 <!-- <li><a href="?command=PLAY_PROGRAM&params=">Play Program</a> 
 <li><a href="?command=STOP_PROGRAM&params=">Stop Program</a> -->
 
-<li><a href="?command=MEMORY_EVENT_RAISE&params=animation"><img src="static/choregraphe-icons/box/movement/sit.png"></a>
-<li><a href="?command=STOP_PROGRAM&params="><img src="static/choregraphe-icons/box/movement/sit.png"></a>
-<li><a href="?command=PLAY_PROGRAM&params="><img src="static/choregraphe-icons/box/movement/sit.png"></a>
-<li><a href="?command=STOP_PROGRAM&params="><img src="static/choregraphe-icons/box/movement/sit.png"></a>
-<li><a href="?command=PLAY_PROGRAM&params="><img src="static/choregraphe-icons/box/movement/sit.png"></a>
-<li><a href="?command=STOP_PROGRAM&params="><img src="static/choregraphe-icons/box/movement/sit.png"></a>
-<li><a href="?command=PLAY_PROGRAM&params="><img src="static/choregraphe-icons/box/movement/sit.png"></a>
-<li><a href="?command=STOP_PROGRAM&params="><img src="static/choregraphe-icons/box/movement/sit.png"></a>
+<li><a href="?command=PLAY_PROGRAM&params=['{\'name\': \'Say\', \'data\': {\'text\':\'Hello I am NAO.\'}}']'><img src="static/choregraphe-icons/box/movement/sit.png"></a>
 </ul>
 
 </body>
