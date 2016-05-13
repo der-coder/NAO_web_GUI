6963b41009c1ae895e40876768f2b87ec21f6bf6
Isaac Ayala <isaac.ayalaiii@gmail.com>
2016-05-12 19:54:45 -0500

    Removed extra lines

--- html/commandMovement.php
+++ html/commandMovement.php
@@ -4,15 +4,11 @@
 <?php include 'banner.php';?>
 <?php include 'comandos.php';?>
 
-<div class="leftside">
-        <div class="homebox">
         <ul id="icons">
 <li><a href="?command=SIT_DOWN&params="><img src="static/choregraphe-icons/box/movement/sit.png"></a>
 <li><a href="?command=STAND_UP&params="><img src="static/choregraphe-icons/box/movement/stand.png"></a>
 <li><a id="Seiza" href="?command=SET_LIFE_STATE&params='disabled'"><img src="static/choregraphe-icons/box/wait.png"></a>
         </ul>
-        </div>
-</div>
 
 </body>
 
