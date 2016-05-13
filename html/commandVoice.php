17f25ce26425d53e773cae72e5737629e05f38dd
Isaac Ayala <isaac.ayalaiii@gmail.com>
2016-05-12 19:02:33 -0500

    Removed home from command menu

--- html/commandVoice.php
+++ html/commandVoice.php
@@ -4,8 +4,15 @@
 <?php include 'banner.php';?>
 <?php include 'comandos.php';?>
 <ul class="voice">
-<li><a href="?command=CMDSAY&params="><img src="static/choregraphe-icons/box/interaction/say.png"></a>
+<li><a href="?command=CMDSAY&params=$parameters"><img src="static/choregraphe-icons/box/interaction/say.png"></a>
 </ul>
+  <form action="foobar_submit.php" method="post">
+    <input name="my_html_input_tag"  value="PILLS HERE"/>
+
+    <input type="submit" name="my_form_submit_button" 
+           value="Click here for penguins"/>
+
+    </form>
 </body>
 
 </html>
