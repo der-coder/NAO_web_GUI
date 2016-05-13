8bfaead5bb56a031a35795a4e17c30e461b8ee15
Isaac Ayala <isaac.ayalaiii@gmail.com>
2016-05-12 19:20:33 -0500

    Voice test x5

--- html/commandVoice.php
+++ html/commandVoice.php
@@ -6,8 +6,8 @@
 <ul class="voice">
 <li><a href="?command=SAY&params=['Hello Bats']"><img src="static/choregraphe-icons/box/interaction/say.png"></a>
 </ul>
-  <form action="?command=SAY&params=['Hello Bats']" method="post">
-    <input name="?command=SAY&params=" value="['hello']"/>
+  <form action="?command=SAY&params=['Hail Satan']" method="post">
+    <input name="?command=SAY&params=" value="['I believe I can fly']"/>
 
     <input type="submit" name="" 
            value=""/>
