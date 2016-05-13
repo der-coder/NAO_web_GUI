ca65bf01361ec0c65af080ca7c8dff980d349827
Isaac Ayala <isaac.ayalaiii@gmail.com>
2016-05-12 19:51:52 -0500

    Removed extra []

--- html/commandProgram.php
+++ html/commandProgram.php
@@ -9,6 +9,9 @@
 <li><a href="?command=STOP_PROGRAM&params=">Stop Program</a> -->
 
 <li><a href="?command=PLAY_PROGRAM&params='{\'name\': \'Say\', \'data\': {\'text\':\'Hello I am NAO.\'}}'"><img src="static/choregraphe-icons/box/movement/sit.png"></a>
+
+<li><a href="?command=PLAY_PROGRAM&params='{\'name\': \'Walk to\', \'data\': {\'theta\':15.0,\'x\':0.2,\'arms\':True,\'y\':0.2}}'"><img src="static/choregraphe-icons/box/movement/sit.png"></a>
+
 </ul>
 
 </body>
