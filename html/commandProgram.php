7fea58080cdbe028bff98011c4dd8273f387fbd2
Isaac Ayala <isaac.ayalaiii@gmail.com>
2016-05-12 20:00:27 -0500

    Removed extra icon formatting

--- html/commandProgram.php
+++ html/commandProgram.php
@@ -10,7 +10,7 @@
 
 <li><a href="?command=PLAY_PROGRAM&params='{\'name\': \'Say\', \'data\': {\'text\':\'Hello I am NAO.\'}}'"><img src="static/choregraphe-icons/box/movement/sit.png"></a>
 
-<li><a href="?command=PLAY_PROGRAM&params='{\'name\': \'Walk to\', \'data\': {\'theta\':15.0,\'x\':0.2,\'arms\':True,\'y\':0.2}}'"><img src="static/choregraphe-icons/box/movement/sit.png"></a>
+<li><a href="?command=PLAY_PROGRAM&params='{\'name\': \'Walk to\', \'data\': {\'theta\':0.0,\'x\':0.0,\'arms\':True,\'y\':0.2}}'"><img src="static/choregraphe-icons/box/movement/sit.png"></a>
 
 </ul>
 
