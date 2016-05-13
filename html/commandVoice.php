cddd8d54ea3e436489cfcc967b5228d4d91b5711
Isaac Ayala <isaac.ayalaiii@gmail.com>
2016-05-12 19:37:05 -0500

    Removed form

--- html/commandVoice.php
+++ html/commandVoice.php
@@ -6,13 +6,13 @@
 <ul class="voice">
 <li><a href="?command=SAY&params=['Hello Bats']"><img src="static/choregraphe-icons/box/interaction/say.png"></a>
 </ul>
-<form action="?command=SAY&params=['I believe I can fly']" method="post">
-<!--<form action="foobar_submit.php" method="post">-->
+<!--<form action="?command=SAY&params=['I believe I can fly']" method="post">
     <input name="parameters" value="['I believe I can fly']"/>
 
     <input type="submit" name="" 
            value=""/>
     </form>
+    -->
 </body>
 
 </html>
