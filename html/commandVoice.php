b270cd70eacbcb0f9f18fe2408314d2b3eddcfbc
Isaac Ayala <isaac.ayalaiii@gmail.com>
2016-05-12 19:23:03 -0500

    Hello Bats

--- html/commandVoice.php
+++ html/commandVoice.php
@@ -6,7 +6,7 @@
 <ul class="voice">
 <li><a href="?command=SAY&params=['Hello Bats']"><img src="static/choregraphe-icons/box/interaction/say.png"></a>
 </ul>
-  <form action="?command=SAY&params=$POST[_parameters]" method="post">
+  <form action="?command=SAY&params=$_POST['parameters']" method="post">
     <input name="parameters" value="['I believe I can fly']"/>
 
     <input type="submit" name="" 
